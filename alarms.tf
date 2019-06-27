16be76dfae7205d6b22efb17eedd0bcacdc21a2c
Charlie Chen <charliechen@exosite.com>
2019-06-27 17:19:46 +0800

    Update wrong "data_description" to sync with AWS guide
    https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/cloudwatch-alarms.html

--- alarms.tf
+++ alarms.tf
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_metric_alarm" "cluster_status_is_red" {
   period              = "60"
   statistic           = "Maximum"
   threshold           = "1"
-  alarm_description   = "Average elasticsearch cluster status is in red over last 5 minutes"
+  alarm_description   = "Average elasticsearch cluster status is in red over last 1 minutes"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
   treat_missing_data  = "ignore"
@@ -40,7 +40,7 @@ resource "aws_cloudwatch_metric_alarm" "cluster_status_is_yellow" {
   period              = "60"
   statistic           = "Maximum"
   threshold           = "1"
-  alarm_description   = "Average elasticsearch cluster status is in yellow over last 5 minutes"
+  alarm_description   = "Average elasticsearch cluster status is in yellow over last 1 minutes"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
   treat_missing_data  = "ignore"
@@ -82,7 +82,7 @@ resource "aws_cloudwatch_metric_alarm" "cluster_index_writes_blocked" {
   period              = "300"
   statistic           = "Maximum"
   threshold           = "1"
-  alarm_description   = "Elasticsearch index writes being blocker over last 10 minutes"
+  alarm_description   = "Elasticsearch index writes being blocker over last 5 minutes"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
   treat_missing_data  = "ignore"
@@ -121,10 +121,10 @@ resource "aws_cloudwatch_metric_alarm" "automated_snapshot_failure" {
   evaluation_periods  = "1"
   metric_name         = "AutomatedSnapshotFailure"
   namespace           = "AWS/ES"
-  period              = "600"
+  period              = "60"
   statistic           = "Maximum"
   threshold           = "1"
-  alarm_description   = "Elasticsearch automated snapshot failed over last 10 minutes"
+  alarm_description   = "Elasticsearch automated snapshot failed over last 1 minutes"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
   treat_missing_data  = "ignore"
@@ -145,7 +145,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
   period              = "900"
   statistic           = "Average"
   threshold           = "${local.thresholds["CPUUtilizationThreshold"]}"
-  alarm_description   = "Average elasticsearch cluster CPU utilization over last 10 minutes too high"
+  alarm_description   = "Average elasticsearch cluster CPU utilization over last 45 minutes too high"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
 
@@ -165,7 +165,7 @@ resource "aws_cloudwatch_metric_alarm" "jvm_memory_pressure_too_high" {
   period              = "900"
   statistic           = "Maximum"
   threshold           = "${local.thresholds["JVMMemoryPressureThreshold"]}"
-  alarm_description   = "Elasticsearch JVM memory pressure is too high over last 10 minutes"
+  alarm_description   = "Elasticsearch JVM memory pressure is too high over last 15 minutes"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
 
@@ -185,7 +185,7 @@ resource "aws_cloudwatch_metric_alarm" "master_cpu_utilization_too_high" {
   period              = "900"
   statistic           = "Average"
   threshold           = "${local.thresholds["MasterCPUUtilizationThreshold"]}"
-  alarm_description   = "Average elasticsearch cluster CPU utilization over last 10 minutes too high"
+  alarm_description   = "Average elasticsearch cluster CPU utilization over last 45 minutes too high"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
 
@@ -205,7 +205,7 @@ resource "aws_cloudwatch_metric_alarm" "master_jvm_memory_pressure_too_high" {
   period              = "900"
   statistic           = "Maximum"
   threshold           = "${local.thresholds["MasterJVMMemoryPressureThreshold"]}"
-  alarm_description   = "Elasticsearch JVM memory pressure is too high over last 10 minutes"
+  alarm_description   = "Elasticsearch JVM memory pressure is too high over last 15 minutes"
   alarm_actions       = ["${local.aws_sns_topic_arn}"]
   ok_actions          = ["${local.aws_sns_topic_arn}"]
 
