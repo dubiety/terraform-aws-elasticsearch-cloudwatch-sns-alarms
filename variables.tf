dc38e3ab896b2e3dc6db141c995e0bf5185f6376
Flip <flip@fliphess.com>
2019-07-17 18:07:26 +0200

    Use boolean types for boolean variables

--- variables.tf
+++ variables.tf
@@ -23,81 +23,79 @@ variable "alarm_name_postfix" {
 
 variable "monitor_cluster_status_is_red" {
   description = "Enable monitoring of cluster status is in red"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_cluster_status_is_yellow" {
   description = "Enable monitoring of cluster status is in yellow"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_free_storage_space_too_low" {
   description = "Enable monitoring of cluster average free storage is to low"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_cluster_index_writes_blocked" {
   description = "Enable monitoring of cluster index writes being blocked"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_insufficient_available_nodes" {
   description = "Enable monitoring insufficient available nodes"
-  type        = "string"
+  type        = bool
   default     = false
 }
 
 variable "monitor_automated_snapshot_failure" {
   description = "Enable monitoring of automated snapshot failure"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_cpu_utilization_too_high" {
   description = "Enable monitoring of CPU utilization is too high"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_jvm_memory_pressure_too_high" {
   description = "Enable monitoring of JVM memory pressure is too high"
-  type        = "string"
+  type        = bool
   default     = true
 }
 
 variable "monitor_master_cpu_utilization_too_high" {
   description = "Enable monitoring of CPU utilization of master nodes are too high. Only enable this when dedicated master is enabled"
-  type        = "string"
+  type        = bool
   default     = false
 }
 
 variable "monitor_master_jvm_memory_pressure_too_high" {
   description = "Enable monitoring of JVM memory pressure of master nodes are too high. Only enable this wwhen dedicated master is enabled"
-  type        = "string"
+  type        = bool
   default     = false
 }
 
 variable "free_storage_space_threshold" {
   description = "The minimum amount of available storage space in MegaByte."
-  type        = "string"
-  default     = 20480
-
-  # 20 Gigabyte in MegaByte
+  type        = number
+  default     = 20480 ## 20 Gigabyte in MegaByte
 }
 
 variable "min_available_nodes" {
   description = "The minimum available (reachable) nodes to have"
-  type        = "string"
+  type        = number
   default     = 1
 }
 
 variable "cpu_utilization_threshold" {
   description = "The maximum percentage of CPU utilization"
-  type        = "string"
+  type        = number
   default     = 80
 
   # 80 percent in Percentage
@@ -105,7 +103,7 @@ variable "cpu_utilization_threshold" {
 
 variable "jvm_memory_pressure_threshold" {
   description = "The maximum percentage of the Java heap used for all data nodes in the cluster"
-  type        = "string"
+  type        = number
   default     = 80
 
   # 80 percent in Percentage
@@ -113,16 +111,16 @@ variable "jvm_memory_pressure_threshold" {
 
 variable "master_cpu_utilization_threshold" {
   description = "The maximum percentage of CPU utilization of master nodes"
-  type        = "string"
-  default     = ""
+  type        = number
+  default     = 80 # default same as `cpu_utilization_threshold` in Percentage
 
-  # default same as `cpu_utilization_threshold` in Percentage
+
 }
 
 variable "master_jvm_memory_pressure_threshold" {
   description = "The maximum percentage of the Java heap used for master nodes in the cluster"
-  type        = "string"
-  default     = ""
+  type        = number
+  default     = 80 # default same as `jvm_memory_pressure_threshold` in Percentage
 
-  # default same as `jvm_memory_pressure_threshold` in Percentage
+
 }
