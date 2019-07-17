c726b6241ccbdb4f8ab1b3145f9fdf8f4d2d53fb
Flip <flip@fliphess.com>
2019-07-17 17:42:45 +0200

    Support v0.12

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "sns_topic_arn" {
   description = "The ARN of the SNS topic"
-  value       = "${local.aws_sns_topic_arn}"
+  value       = local.aws_sns_topic_arn
 }
 
 output "sns_topic_name" {
   description = "The SNS topic name"
-  value       = "${local.aws_sns_topic_name}"
+  value       = local.aws_sns_topic_name
 }
