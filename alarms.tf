095d66a3c703e628c068e4de0ccdb1fc88ac8ddb
Farley <farley@neonsurge.com>
2021-12-27 16:10:37 +1300

    make feature work

--- alarms.tf
+++ alarms.tf
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_metric_alarm" "cluster_status_is_red" {
   count               = var.monitor_cluster_status_is_red ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-ClusterStatusIsRed${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_cluster_status_is_red_periods
   metric_name         = "ClusterStatus.red"
   namespace           = "AWS/ES"
   period              = var.monitor_cluster_status_is_red_period
@@ -79,7 +79,7 @@ resource "aws_cloudwatch_metric_alarm" "cluster_index_writes_blocked" {
   count               = var.monitor_cluster_index_writes_blocked ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-ClusterIndexWritesBlocked${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_cluster_index_writes_blocked_periods
   metric_name         = "ClusterIndexWritesBlocked"
   namespace           = "AWS/ES"
   period              = var.monitor_cluster_index_writes_blocked_period
@@ -101,7 +101,7 @@ resource "aws_cloudwatch_metric_alarm" "insufficient_available_nodes" {
   count               = var.min_available_nodes > 0 ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-InsufficientAvailableNodes${var.alarm_name_postfix}"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_min_available_nodes_periods
   metric_name         = "Nodes"
   namespace           = "AWS/ES"
   period              = var.monitor_min_available_nodes_period
@@ -123,7 +123,7 @@ resource "aws_cloudwatch_metric_alarm" "automated_snapshot_failure" {
   count               = var.monitor_automated_snapshot_failure ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-AutomatedSnapshotFailure${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_automated_snapshot_failure_periods
   metric_name         = "AutomatedSnapshotFailure"
   namespace           = "AWS/ES"
   period              = var.monitor_automated_snapshot_failure_period
@@ -145,7 +145,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
   count               = var.monitor_cpu_utilization_too_high ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-CPUUtilizationTooHigh${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "3"
+  evaluation_periods  = var.monitor_cpu_utilization_too_high_periods
   metric_name         = "CPUUtilization"
   namespace           = "AWS/ES"
   period              = var.monitor_cpu_utilization_too_high_period
@@ -166,7 +166,7 @@ resource "aws_cloudwatch_metric_alarm" "jvm_memory_pressure_too_high" {
   count               = var.monitor_jvm_memory_pressure_too_high ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-JVMMemoryPressure${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_jvm_memory_pressure_too_high_periods
   metric_name         = "JVMMemoryPressure"
   namespace           = "AWS/ES"
   period              = var.monitor_jvm_memory_pressure_too_high_period
@@ -187,7 +187,7 @@ resource "aws_cloudwatch_metric_alarm" "master_cpu_utilization_too_high" {
   count               = var.monitor_master_cpu_utilization_too_high ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-MasterCPUUtilizationTooHigh${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "3"
+  evaluation_periods  = var.monitor_master_cpu_utilization_too_high_periods
   metric_name         = "MasterCPUUtilization"
   namespace           = "AWS/ES"
   period              = var.monitor_master_cpu_utilization_too_high_period
@@ -208,7 +208,7 @@ resource "aws_cloudwatch_metric_alarm" "master_jvm_memory_pressure_too_high" {
   count               = var.monitor_master_jvm_memory_pressure_too_high ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-MasterJVMMemoryPressure${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_master_jvm_memory_pressure_too_high_periods
   metric_name         = "MasterJVMMemoryPressure"
   namespace           = "AWS/ES"
   period              = var.monitor_master_jvm_memory_pressure_too_high_period
@@ -229,7 +229,7 @@ resource "aws_cloudwatch_metric_alarm" "kms_key_error" {
   count               = var.monitor_kms ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-KMSKeyError${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_kms_periods
   metric_name         = "KMSKeyError"
   namespace           = "AWS/ES"
   period              = var.monitor_kms_period
@@ -251,7 +251,7 @@ resource "aws_cloudwatch_metric_alarm" "kms_key_inaccessible" {
   count               = var.monitor_kms ? 1 : 0
   alarm_name          = "${var.alarm_name_prefix}ElasticSearch-KMSKeyInaccessible${var.alarm_name_postfix}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
+  evaluation_periods  = var.monitor_kms_periods
   metric_name         = "KMSKeyInaccessible"
   namespace           = "AWS/ES"
   period              = var.monitor_kms_period
